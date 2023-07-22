f5fde1afc897161d48a5b4715f780b42cf69af1e
nitesh01487 <niteshkumar01487@gmail.com>
2023-07-22 23:18:55 +0530

    updated layout for mobile

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -29342,7 +29342,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59450" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49524" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
